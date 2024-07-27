c39cac42868472090285340591f426dcbfc37f7a
Daniel B. <boger.daniel@outlook.de>
2024-07-27 19:38:50 +0200

    RewardsLevelingMagic has been filled and drumroll works for it

--- src/main/java/user/meistertisch/danissmpplugin/level/invs/drumroll/InventoryDrumroll.java
+++ src/main/java/user/meistertisch/danissmpplugin/level/invs/drumroll/InventoryDrumroll.java
@@ -19,6 +19,7 @@ import user.meistertisch.danissmpplugin.level.types.adventure.RewardsLevelingAdv
 import user.meistertisch.danissmpplugin.level.types.building.RewardsLevelingBuilding;
 import user.meistertisch.danissmpplugin.level.types.combat.RewardsLevelingCombat;
 import user.meistertisch.danissmpplugin.level.types.farming.RewardsLevelingFarming;
+import user.meistertisch.danissmpplugin.level.types.magic.RewardsLevelingMagic;
 import user.meistertisch.danissmpplugin.level.types.mining.RewardsLevelingMining;
 import user.meistertisch.danissmpplugin.level.types.trading.RewardsLevelingTrading;
 
@@ -366,6 +367,60 @@ public class InventoryDrumroll {
                 }, 0, 1);
 
             }
+            case MAGIC -> {
+                RewardsLevelingMagic[] rewards = new RewardsLevelingMagic[9];
+                player.openInventory(inv);
+
+                task = Bukkit.getScheduler().runTaskTimer(Main.getPlugin(), () -> {
+                    count++;
+                    int period = 1;
+
+                    if(count >= 60 && count <= 100){
+                        period = 2;
+                    }
+                    if(count >= 100 && count <= 140){
+                        period = 5;
+                    }
+                    if(count >= 140 && count <= 180){
+                        period = 10;
+                    }
+                    if(count >= 180 && count <= 200){
+                        period = 20;
+                    }
+                    if(count >= 200){
+                        period = 9999;
+                    }
+                    if(count >= 220){
+                        task.cancel();
+                        givePlayerReward(rewards[4]);
+                        return;
+                    }
+//                    player.openInventory(inv);
+
+                    if(count % period == 0) {
+                        RewardsLevelingMagic lastReward = RewardsLevelingMagic.getNextItem();
+                        for (int i = 0; i < 9; i++) {
+                            RewardsLevelingMagic thisReward = rewards[i];
+                            rewards[i] = lastReward;
+                            lastReward = thisReward;
+                        }
+
+                        for (int i = 0; i < 9; i++) {
+                            RewardsLevelingMagic reward = rewards[i];
+                            if(reward == null) continue;
+
+                            ItemStack item = new ItemStack(reward.getMaterial(), reward.getAmount());
+                            ItemMeta meta = item.getItemMeta();
+                            meta.displayName(Component.text(bundle.getString(reward.getName())).color(reward.getRarity().getColor()));
+                            meta.lore(List.of(Component.text(bundle.getString(reward.getDescription()))));
+                            item.setItemMeta(meta);
+
+                            inv.setItem(17 - i, item);
+                        }
+                    }
+                }, 0, 1);
+
+            }
         }
 
 
@@ -415,6 +470,13 @@ public class InventoryDrumroll {
                     .color(reward.getRarity().getColor())
                     .decoration(TextDecoration.BOLD, true);
         }  else if(obj instanceof RewardsLevelingFarming reward){
+            rarity = Component.text(bundle.getString(reward.getRarity().getName()))
+                    .color(reward.getRarity().getColor())
+                    .decoration(TextDecoration.BOLD, true);
+            itemName = item.displayName()
+                    .color(reward.getRarity().getColor())
+                    .decoration(TextDecoration.BOLD, true);
+        } else if(obj instanceof RewardsLevelingMagic reward){
             rarity = Component.text(bundle.getString(reward.getRarity().getName()))
                     .color(reward.getRarity().getColor())
                     .decoration(TextDecoration.BOLD, true);
--- src/main/java/user/meistertisch/danissmpplugin/level/types/magic/RewardsLevelingMagic.java
+++ src/main/java/user/meistertisch/danissmpplugin/level/types/magic/RewardsLevelingMagic.java
@@ -1,4 +1,71 @@
 package user.meistertisch.danissmpplugin.level.types.magic;
 
+import org.bukkit.Material;
+import user.meistertisch.danissmpplugin.level.invs.drumroll.WinRarityTypes;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public enum RewardsLevelingMagic {
+    LAPIS("level.magic.rewards.lapis.name", "level.magic.rewards.lapis.desc", Material.LAPIS_LAZULI, 32, WinRarityTypes.COMMON),
+    MANY_LAPIS("level.magic.rewards.many_lapis.name", "level.magic.rewards.many_lapis.desc", Material.LAPIS_LAZULI, 64, WinRarityTypes.UNCOMMON),
+    BOOKS("level.magic.rewards.books.name", "level.magic.rewards.books.desc", Material.BOOK, 16, WinRarityTypes.UNCOMMON),
+    ENDER_PEARL("level.magic.rewards.ender_pearl.name", "level.magic.rewards.ender_pearl.desc", Material.ENDER_PEARL, 16, WinRarityTypes.RARE),
+    MANY_BOOKS("level.magic.rewards.many_books.name", "level.magic.rewards.many_books.desc", Material.BOOK, 32, WinRarityTypes.RARE),
+    ENCHANTING_TABLE("level.magic.rewards.enchanting_table.name", "level.magic.rewards.enchanting_table.desc", Material.ENCHANTING_TABLE, 1, WinRarityTypes.RARE),
+    BOTTLE_O_ENCHANTING("level.magic.rewards.bottle_o_enchanting.name", "level.magic.rewards.bottle_o_enchanting.desc", Material.EXPERIENCE_BOTTLE, 16, WinRarityTypes.EPIC),
+    MANY_BOTTLES_O_ENCHANTING("level.magic.rewards.many_bottle_o_enchanting.name", "level.magic.rewards.many_bottle_o_enchanting.desc", Material.EXPERIENCE_BOTTLE, 32, WinRarityTypes.LEGENDARY);
+
+    private final String name;
+    private final String description;
+    private final Material material;
+    private final int amount;
+    private final WinRarityTypes rarity;
+
+    RewardsLevelingMagic(String name, String description, Material material, int amount, WinRarityTypes rarity) {
+        this.name = name;
+        this.description = description;
+        this.material = material;
+        this.amount = amount;
+        this.rarity = rarity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public WinRarityTypes getRarity() {
+        return rarity;
+    }
+
+    public static List<RewardsLevelingMagic> getRewards(WinRarityTypes rarity){
+        List<RewardsLevelingMagic> list = new ArrayList<>();
+
+        for(RewardsLevelingMagic reward : RewardsLevelingMagic.values()){
+            if(reward.getRarity().equals(rarity)){
+                list.add(reward);
+            }
+        }
+
+        return list;
+    }
+
+    public static RewardsLevelingMagic getNextItem(){
+        WinRarityTypes rarity = WinRarityTypes.getRandomRarity();
+        List<RewardsLevelingMagic> rewards = getRewards(rarity);
+
+        return rewards.get((int) (Math.random() * rewards.size()));
+    }
 }
