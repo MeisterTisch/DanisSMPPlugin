c65dc4df4fc0008cefafaf0eb6ba6c67ea8d4630
Daniel B. <boger.daniel@outlook.de>
2024-10-27 12:22:16 +0100

    Bug removed and Updated to 0.4

--- src/main/java/user/meistertisch/danissmpplugin/admin/misc/CommandTest.java
+++ src/main/java/user/meistertisch/danissmpplugin/admin/misc/CommandTest.java
@@ -1,14 +1,11 @@
 package user.meistertisch.danissmpplugin.admin.misc;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import user.meistertisch.danissmpplugin.Main;
 import user.meistertisch.danissmpplugin.admin.spawn.ManagerSpawn;
-import user.meistertisch.danissmpplugin.admin.spawn.ParticlesSpawnBorder;
 
 public class CommandTest implements CommandExecutor {
     @Override
